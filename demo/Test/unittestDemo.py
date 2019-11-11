f79c383ce83a5fbe9f92ab288c6198027bc22d0b
lfeng <lfengnl@163.com>
2019-11-11 23:41:42 +0800

    单元测试添加

---
+++ demo/Test/unittestDemo.py
@@ -0,0 +1,34 @@
+import unittest
+
+class TestSuiteMethods(unittest.TestSuite):
+    pass
+
+
+class TestStringMethods(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownclass')
+
+    def setUp(self):
+        print('setUp')
+
+    def tearDown(self):
+        print('tearDown')
+
+    def test_upper(self):
+        print('test_upper')
+        self.assertEqual('foo'.upper(), "FOO")
+
+    def test_split(self):
+        print('test_split')
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+
+
+if __name__ == "__main__":
+    unittest.main()
